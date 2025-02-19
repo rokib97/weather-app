18b28d63af60c2bee34f48f1135d0e87cc0bc10a
rokib97 <rokibulhasan.ph@gmail.com>
2025-02-19 20:38:22 +0600

    completed project with nice formatted output

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os 
+from pprint import pprint
 
 load_dotenv()
 
@@ -8,6 +9,12 @@ def get_current_weather():
     print("\n*** Get Current Weather Condition ***\n")
     city = input("\nPlease enter a city name:\n")
     request_url = f'https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv("API_KEY")}&q={city}&units=metric'
-    print(request_url)
     
-get_current_weather()
+    weather_data = requests.get(request_url).json()
+    pprint(weather_data)
+    print(f"\nCurrent Weather for {weather_data['name']}")
+    print(f"\nThe temp is {weather_data['main']['temp']}")
+    print(f"\nFeels like {weather_data['main']['feels_like']} and {weather_data['weather'][0]['description']}")
+    
+if __name__ == "__main__":
+    get_current_weather()
